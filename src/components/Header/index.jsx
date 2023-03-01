2abf366be3e8afa151d9cbb0a96c2aef3d43e666
CJ <ngassajason@yahoo.fr>
2023-03-01 21:09:23 +0100

    adds Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import Header_logo from '../../assets/img/kasa_header_logo.svg';
+import { NavLink, useLocation } from "react-router-dom";
+import './_Header.scss';
+
+const Header = () => {
+ 
+  return (
+    <header className='header space-between align-center'>
+      <NavLink className='header_img_wrapper' to='/'>
+        <img src={Header_logo} alt="Kasa Header Logo" />
+      </NavLink>
+      <nav className='nav__links flex-row space-evenly'>
+        <NavLink to="/" className={ ({isActive}) => isActive ? 'active nav__link' :'nav__link'} >Accueil</NavLink>
+        <NavLink to="/about" className={ ({isActive}) => isActive ? 'active nav__link' :'nav__link'}>A Propos</NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
